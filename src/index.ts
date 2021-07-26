069372cb2fb80220be587bfecde6e58d1f6fef06
Fernando Santos <fernandosantos511@yahoo.com.br>
2021-07-26 08:51:04 -0300

    function to get time

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const _getTime = (): Array<number> => {
+  const date: Date = new Date();
+
+  let hours: number = date.getHours();
+  let minutes: number = date.getMinutes();
+  let seconds: number = date.getSeconds();
+
+  return [hours, minutes, seconds];
+};
