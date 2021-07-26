fa1e71c5ee862a23e01f5c24dbdce019df793f54
Fernando Santos <fernandosantos511@yahoo.com.br>
2021-07-26 09:12:08 -0300

    final project

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,49 @@ const _getTime = (): Array<number> => {
 
   return [hours, minutes, seconds];
 };
+
+const _getTimeSectionComponents = (section: number): Array<string> => {
+  return section.toString().padStart(2, "0").split("");
+};
+
+const _convertComponentsToBinary = (
+  components: Array<string>
+): Array<string> => {
+  return components.map((component: string) => {
+    return Number(component).toString(2).padStart(4, "0");
+  });
+};
+
+const _printToConsole = (components: Array<Array<string>>) => {
+  console.clear();
+
+  for (let i = 0; i < 4; i++) {
+    let row: string = "";
+    for (let j = 0; j < 3; j++) {
+      row += components[j][0][i] + " ";
+      row += components[j][1][i] + " ";
+    }
+
+    console.log(row);
+  }
+};
+
+setInterval(() => {
+  let [hours, minutes, seconds] = _getTime();
+
+  let hourComponents = _getTimeSectionComponents(hours);
+  let minuteComponents = _getTimeSectionComponents(minutes);
+  let secondComponents = _getTimeSectionComponents(seconds);
+
+  let hourBinaryComponets = _convertComponentsToBinary(hourComponents);
+  let minuteBinaryComponents = _convertComponentsToBinary(minuteComponents);
+  let secondBinaryComponents = _convertComponentsToBinary(secondComponents);
+
+  let components = [
+    hourBinaryComponets,
+    minuteBinaryComponents,
+    secondBinaryComponents,
+  ];
+
+  _printToConsole(components);
+}, 1000);
